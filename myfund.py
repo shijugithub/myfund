4ca25cc2094d1be731e267e4f03e24f4d25fbd70
Shiju Varghese <shiju.mail@gmail.com>
2019-08-10 17:49:15 +0000

    update

--- myfund.py
+++ myfund.py
@@ -1,19 +1,36 @@
 #!/usr/bin/python
 
 def main():
+   print("You are in the main function")
    print("Hello World")
    theVar= "This is cool"
    print(theVar)
+   print("")
 
 def theAddFn(var1, var2):
+   print("You entered theAddFn function")
    theResult=var1 + var2
    print(theResult)
+   print("")
 
 def getTheVarNadd():
-   var1=int(input("Enter the first integer"))
-   var2=int(input("Enter the secornd integer"))
+   print("You entered getTheVarNadd function")
+   var1=int(input("Enter the first integer : "))
+   if(var1 == 1):
+      var1=10
+      print("Since 1st integer is 1, it will be changed to 10")
+   print("")
+   var2=int(input("Enter the secornd integer : "))
    theResult= var1 + var2
    print(theResult)
+   print("")
+
+def theWhileLoop():
+   theValue="Good"
+   while (theValue != "Exit"):
+      theValue=raw_input("Enter Exit to exit: ")
+      print(theValue)
+      print("")
 
 
 main()
@@ -21,3 +38,5 @@ theVar= "Cool it is"
 print(theVar)
 theAddFn(1,2)
 getTheVarNadd()
+theWhileLoop()
+
