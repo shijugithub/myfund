bcf86fd348d428939937e6b7f4203a7eda53f894
Shiju Varghese <shiju.mail@gmail.com>
2019-08-10 16:56:43 +0000

    Still learning

--- myfund.py
+++ myfund.py
@@ -1,4 +1,23 @@
-#!/usr/bin/pythom
+#!/usr/bin/python
 
 def main():
    print("Hello World")
+   theVar= "This is cool"
+   print(theVar)
+
+def theAddFn(var1, var2):
+   theResult=var1 + var2
+   print(theResult)
+
+def getTheVarNadd():
+   var1=int(input("Enter the first integer"))
+   var2=int(input("Enter the secornd integer"))
+   theResult= var1 + var2
+   print(theResult)
+
+
+main()
+theVar= "Cool it is"
+print(theVar)
+theAddFn(1,2)
+getTheVarNadd()
